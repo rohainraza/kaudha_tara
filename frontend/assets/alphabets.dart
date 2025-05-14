d625a9f470be3084e2f5a248b49179bdf90aa1c9
Devjoy-chakma <165554234+Devjoy-chakma@users.noreply.github.com>
2025-05-15 08:35:50 +1000

    added alphabets and audio

---
+++ frontend/assets/alphabets.dart
@@ -0,0 +1,84 @@
+import 'package:kaudhatara/models/kaudha.dart';
+
+const alphabets = [
+  Kaudha(
+      kaudha: '𑄶',
+      maleSoundUrl: 'audios/male/00_0_sunnyo.mp3',
+      femaleSoundUrl: 'audios/female/00_0_sunnyo.mp3'),
+  Kaudha(
+      kaudha: '𑄷',
+      maleSoundUrl: 'audios/male/00_1_ek.mp3',
+      femaleSoundUrl: 'audios/female/00_1_ek.mp3'),
+  Kaudha(
+      kaudha: '𑄸',
+      maleSoundUrl: 'audios/male/00_2_dwi.mp3',
+      femaleSoundUrl: 'audios/female/00_2_dwi.mp3'),
+  Kaudha(
+      kaudha: '𑄹',
+      maleSoundUrl: 'audios/male/00_3_tin.mp3',
+      femaleSoundUrl: 'audios/female/00_3_tin.mp3'),
+  Kaudha(
+      kaudha: '𑄺',
+      maleSoundUrl: 'audios/male/00_4_cheir.mp3',
+      femaleSoundUrl: 'audios/female/00_4_cheir.mp3'),
+  Kaudha(
+      kaudha: '𑄻',
+      maleSoundUrl: 'audios/male/00_5_paach.mp3',
+      femaleSoundUrl: 'audios/female/00_5_paach.mp3'),
+  Kaudha(
+      kaudha: '𑄼',
+      maleSoundUrl: 'audios/male/00_6_chaw.mp3',
+      femaleSoundUrl: 'audios/female/00_6_chaw.mp3'),
+  Kaudha(
+      kaudha: '𑄽',
+      maleSoundUrl: 'audios/male/00_7_saat.mp3',
+      femaleSoundUrl: 'audios/female/00_7_saat.mp3'),
+  Kaudha(
+      kaudha: '𑄾',
+      maleSoundUrl: 'audios/male/00_8_aittyo.mp3',
+      femaleSoundUrl: 'audios/female/00_8_aittyo.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/00_9_naw.mp3',
+      femaleSoundUrl: 'audios/female/00_9_naw.mp3'),
+  Kaudha(
+      kaudha: '𑄃',
+      maleSoundUrl: 'audios/male/01_01_pich_pujo_aa.mp3',
+      femaleSoundUrl: 'audios/female/01_01_pich_pujo_aa.mp3'),
+  Kaudha(
+      kaudha: '𑄄',
+      maleSoundUrl: 'audios/male/01_02_dheil_bhanga_e.mp3',
+      femaleSoundUrl: 'audios/female/01_02_dheil_bhanga_e.mp3'),
+  Kaudha(
+      kaudha: '𑄅',
+      maleSoundUrl: 'audios/male/01_03_bochchi_u.mp3',
+      femaleSoundUrl: 'audios/female/01_03_bochchi_u.mp3'),
+  Kaudha(
+      kaudha: '𑄆',
+      maleSoundUrl: 'audios/male/01_04_lejubo_e.mp3',
+      femaleSoundUrl: 'audios/female/01_04_lejubo_e.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/01_05_bajonye_o.mp3',
+      femaleSoundUrl: 'audios/female/01_05_bajonye_o.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/02_01_chuchengye_kaa.mp3',
+      femaleSoundUrl: 'audios/female/02_01_chuchengye_kaa.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/02_02_gujongye_khaa.mp3',
+      femaleSoundUrl: 'audios/female/02_02_gujongye_khaa.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/02_03_chaindye_gaa.mp3',
+      femaleSoundUrl: 'audios/female/02_03_chaindye_gaa.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/02_04_tindeilye_ghaa.mp3',
+      femaleSoundUrl: 'audios/female/02_04_tindeilye_ghaa.mp3'),
+  Kaudha(
+      kaudha: '𑄿',
+      maleSoundUrl: 'audios/male/02_05_chilemo_ngya.mp3',
+      femaleSoundUrl: 'audios/female/02_05_chilemo_ngya.mp3'),
+];
